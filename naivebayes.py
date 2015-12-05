cbfd89ef4aa6ae5a2238914a3b9c13d1050d685c
SCRN <screensaver7@gmail.com>
2015-12-05 13:59:42 -0500

    added more classification measures
    added confusion matrix. precision, recall, not weighted average

--- naivebayes.py
+++ naivebayes.py
@@ -23,7 +23,6 @@ def openCSV(filename, k):
     del line[0]
     col_count = len(line[0])
     row_count = len(line)
-    size = row_count
     kparts = []
     #3 dimensional array
     #kparts[fold#][attribute][class label]
@@ -56,7 +55,7 @@ def openCSV(filename, k):
     csvfile.close()
     #delete test data in training partition
     del kparts[test]
-    return kparts, testdata
+    return kparts, testdata, row_count
 
 def mean(listOfNum):
     if (len(listOfNum) == 0): return 0
@@ -74,54 +73,81 @@ def gaussianPdf(avg, standDev ,x):
            (2*standDev*standDev))
 
 def main(arg):
-    seed(datetime.now())
-    data = openCSV(arg[0], int(arg[1]))
-    test = data[1]
-    data = data[0]
-    cols_count = len(test[0])
-    rows_count = len(test)
-    keys = data[0][0].keys()
-    train = []
-    for attribs in range(cols_count - 1):
-        train.append(dict(zip(keys, [[] for x in range(len(keys))])))
-    #combine testing folds
-    for parts in data:
-        for i in range(cols_count - 1):
-            for word in parts[i]:
-                for array in parts[i][word]:
-                    train[i][word].append(array)
-    del data
-    #calculate mean and stddev first
-    for attrib in train:
-        for key in keys:
-            newval = [mean(attrib[key]), stddev(attrib[key])]
-            attrib[key] = newval
-    #test training
-    count = 0
-    for t in test:
-        prob = dict(zip(keys, [[] for x in range(len(keys))]))
-        for i in range(cols_count - 1):
-            for k in keys:
-                attrib_prob = gaussianPdf(train[i][k][0],
-                                          train[i][k][1],
-                                          float(t[i]))
-                if (i == 0):
-                    prob[k] = attrib_prob
-                else:
-                    prob[k] = prob[k] * attrib_prob
-        total = sum(prob.values())
-        for k in keys:
-            prob[k] = prob[k] / total
-
-        #best match, actual class
-        if (max(prob.iteritems(), key=operator.itemgetter(1))[0] ==
-            t[cols_count - 1]): count+=1
-    #accuracy for one run
-    return count/float(rows_count)
-
-if __name__ == "__main__":
-    results = []
-    for i in range(int(argv[2])):
+    #confusion matrix
+    confmat = []
+    size = 0
+    #runs k times
+    for i in range(int(arg[1])):
         print '%d...' % (i+1),
-        results.append(main(argv[1:]))
-    print '\nAccuracy: %.04f' % (sum(results)/float(len(results)))
+        seed(datetime.now())
+        data = openCSV(arg[0], int(arg[1]))
+        test = data[1]
+        size = data[2]
+        data = data[0]
+        cols_count = len(test[0])
+        rows_count = len(test)
+        keys = data[0][0].keys()
+        #initialize confmat at beginning
+        if (len(confmat) == 0):
+            confmat = [[0 for x in range(len(keys))] for x in range(len(keys))]
+        train = []
+        for attribs in range(cols_count - 1):
+            train.append(dict(zip(keys, [[] for x in range(len(keys))])))
+        #combine testing folds
+        for parts in data:
+            for i in range(cols_count - 1):
+                for word in parts[i]:
+                    for array in parts[i][word]:
+                        train[i][word].append(array)
+        del data
+        #calculate mean and stddev first
+        for attrib in train:
+            for key in keys:
+                newval = [mean(attrib[key]), stddev(attrib[key])]
+                attrib[key] = newval
+
+        #test training set
+        count = 0
+        for t in test:
+            prob = dict(zip(keys, [[] for x in range(len(keys))]))
+            for i in range(cols_count - 1):
+                for k in keys:
+                    attrib_prob = gaussianPdf(train[i][k][0],
+                                              train[i][k][1],
+                                              float(t[i]))
+                    if (i == 0):
+                        prob[k] = attrib_prob
+                    else:
+                        prob[k] = prob[k] * attrib_prob
+            total = sum(prob.values())
+            for k in keys:
+                prob[k] = prob[k] / total
+
+            lblpredict = max(prob.iteritems(), key=operator.itemgetter(1))[0]
+            lblactual = t[cols_count - 1]
+            #rows: actual, columns: predicted
+            confmat[keys.index(lblactual)][keys.index(lblpredict)] += 1
+
+    #error measures
+    tp = 0
+    for i in range(len(keys)):
+        tp += confmat[i][i]
+
+    precision = []
+    for i in range(len(keys)):
+        precision.append(confmat[i][i]/
+                         float(confmat[i][i]+
+                               sum([row[i] for row in confmat])))
+    recall = []
+    for i in range(len(keys)):
+        recall.append(confmat[i][i]/
+                      float(confmat[i][i]+
+                            sum(confmat[i])))
+    acc = tp/float(size)
+    print ('\navg precision: %0.3f\n' +
+           'avg recall: %0.3f\n' +
+           'accuracy: %0.3f') % (mean(precision),
+                                 mean(recall), acc)
+    
+if __name__ == "__main__":
+    main(argv[1:])
